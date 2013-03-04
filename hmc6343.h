0269c899018e01a57b30e9cc878d74e6c5b1eb8f
Nikolai Ovesen <nikolai.ovesen@gmail.com>
2013-03-04 04:29:32 +0100

    Update hmc6343.h

--- hmc6343.h
+++ hmc6343.h
@@ -14,7 +14,7 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 Lesser General Public License for more details.
 
 You should have received a copy of the GNU General Public License
-    along with this software. If not, see <http://www.gnu.org/licenses/>.
+    along with this library. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #ifndef HMC6343_H
